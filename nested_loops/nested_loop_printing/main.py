a24700c4f600176a972b0921e05d8c13009aaaf7
felix8877 <>
2026-05-11 04:39:07 +0000

    Nested loop printing

---
+++ nested_loops/nested_loop_printing/main.py
@@ -0,0 +1,18 @@
+# List of trips
+trips = [['france', 'germany', 'italy', 'spain', 'netherlands', 'sweden', 'norway', 'switzerland', 'austria', 'portugal', 'belgium'],
+         ['japan', 'china', 'thailand', 'vietnam', 'ndonesia', 'india', 'malaysia', 'philippines', 'singapore', 'mongolia'],
+         ['usa', 'canada', 'mexico', 'brazil', 'argentina', 'colombia', 'peru', 'chile', 'ecuador'],
+         ['egypt', 'morocco', 'south africa', 'tunisia', 'algeria', 'kenya', 'nigeria', 'ethiopia'],
+         ['australia', 'new zealand', 'fiji', 'papua new guinea', 'samoa']]
+
+# List of all countries 
+countries = []
+for sub in trips:
+    for contr in sub:        
+        countries.append(contr.capitalize())
+        
+
+
+
+# Testing
+print('List of Countries:', countries)
